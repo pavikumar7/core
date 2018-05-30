f69eaa323d1fd9a5b9ebb34d5ac68b0b6fcccb14
pavikumar <pavithara810@gmail.com>
2018-05-30 12:12:01 +0530

    switch code

---
+++ corejava/src/corejava/ExmpleSwitch.java
@@ -0,0 +1,42 @@
+package corejava;
+
+public class ExmpleSwitch {
+	public static void main(String b[])
+	{
+		int  count=5;
+		switch(count)
+		{
+		case 1:
+		{
+			System.out.println("one");
+			break;
+		}
+		case 2:
+		{
+			System.out.println("two");
+			break;
+		}
+		case 3:
+		{
+			System.out.println("three");
+			break;
+		}
+		case 4:
+		{
+			System.out.println("four");
+			break;
+		}
+		case 5:
+		{
+			System.out.println("five");
+			break;
+		}
+		default:
+		{
+			System.out.println("default");
+		}
+
+		}
+	}
+
+}
