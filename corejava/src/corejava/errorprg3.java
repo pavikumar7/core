1ee242d44359b981a0b1dec4940e25b562f64849
pavikumar <pavithara810@gmail.com>
2018-05-30 14:28:17 +0530

    sample3

---
+++ corejava/src/corejava/errorprg3.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class errorprg3 {
+	public static void main(String a[])
+	{
+		int total=0,x=1;
+		while ( x <= 100 )
+		{
+			
+		
+			total += x;
+			++x;
+       System.out.println(total);
+	}
+		
+	}	
+
+}
