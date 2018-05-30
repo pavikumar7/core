84a148ae822facf7a7ca77bc74b4a2d95ea367da
pavikumar <pavithara810@gmail.com>
2018-05-30 15:31:06 +0530

    code

---
+++ corejava/src/corejava/exampledowhlie.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class exampledowhlie {
+	public static void main(String c[])
+	{
+		int i=0;
+		do {
+			System.out.println("i= "+i);
+			i++;
+		}while(i<=20);
+	}
+
+}
