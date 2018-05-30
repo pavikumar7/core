af2fa33de6d79e7dbf7a21bcc655c6933d158af8
pavikumar <pavithara810@gmail.com>
2018-05-30 12:00:10 +0530

    condition code

---
+++ corejava/src/corejava/conditionExample.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class conditionExample {
+	public static void main(String a[])
+	{
+		   int n1=20,n2=63;
+		   int avg;
+		   avg=(n1>n2)?n1:n2;
+	System.out.println(avg); 
+	}
+	
+
+}
