737de5110842a22f0773593ccf0e540fd9d75b34
pavikumar <pavithara810@gmail.com>
2018-05-30 10:58:46 +0530

    if statement

---
+++ ifstat.java
@@ -0,0 +1,21 @@
+package javacore;
+
+public class ifstat {
+	public static void main(String arg[])
+	{
+		int a =10;
+		if(a%2==0)
+		{
+			
+		
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Even");
+		}
+		
+	}
+		
+
+}
