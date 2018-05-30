9287099a74479165be7cb3048df122ed309f45bb
pavikumar <pavithara810@gmail.com>
2018-05-30 15:04:21 +0530

    code example

---
+++ corejava/src/corejava/Mystery2.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class Mystery2 {
+	public static void main( String args[] )
+	{
+	int count = 1;
+
+	while ( count <= 10 )
+	{
+	 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+	 ++count;
+	 } // end while
+	 // end main
+	 } 
+
+
+}
