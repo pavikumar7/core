fe356a7b7adf936480ebf9b36d46a7c8fd6edd6a
pavikumar <pavithara810@gmail.com>
2018-05-30 12:33:21 +0530

    while sample code

---
+++ corejava/src/corejava/Examplewhile.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Examplewhile {
+	public static void main(String n[])
+	{
+		int a=0;
+		while(a<15)
+		{
+			System.out.println("value a="+a);
+			a++;
+		}
+	}
+
+}
