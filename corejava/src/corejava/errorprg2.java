92cb59b454b11cd5f9c6e4a3629808a966ed13a7
pavikumar <pavithara810@gmail.com>
2018-05-30 14:13:25 +0530

    sample 1

---
+++ corejava/src/corejava/errorprg2.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class errorprg2 {
+	public static void main(String p[])
+	{
+	 int x = 1, total=0;
+	while ( x <= 10 )
+	{
+	total += x;
+	++x;
+	System.out.println(total);
+	}
+
+
+	}
+
+
+}
