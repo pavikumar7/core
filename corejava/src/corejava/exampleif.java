371a96b32ee2dc8a5d0cf3bba46dbc3c44bd14c5
pavikumar <pavithara810@gmail.com>
2018-05-30 12:47:33 +0530

    example code

---
+++ corejava/src/corejava/exampleif.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class exampleif {
+	public static void main(String a[])
+	{ 
+		int age=45;
+		 if( age >= 65 )
+		 {
+		System.out.println( "Age is greater than or equal to 65" );
+		 }
+		 else
+		 {
+		System.out.println( "Age is less than 65 ");
+		 }
+
+	}
+
+}
